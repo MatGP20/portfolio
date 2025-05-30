180b1da6fb14a632e129528ae933d838aca77e7b
Matias <mgabrielpalacio@gmail.com>
2025-05-30 18:23:22 -0300

    Layout y Home

---
+++ Blazor-Portfolio/BlazorWASMPortfolio/Layout/NavMenu.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWASMPortfolio.Layout
+{
+    public partial class NavMenu : ComponentBase
+    {
+        private string? pageTittle;
+
+        private bool collapseNavMenu = true;
+
+        private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+
+        [Inject] private NavigationManager navMgr {  get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ChangeTittle();
+        }
+
+        private async Task ChangeTittle()
+        {
+            await Task.Delay(500);
+            var pageUri = navMgr.Uri.Split("/", StringSplitOptions.RemoveEmptyEntries);
+            switch (pageUri[pageUri.Length - 1])
+            {
+                case "aboutme":
+                    pageTittle = "ABOUT ME";
+                    break;
+                case "skills":
+                    pageTittle = "SKILLS";
+                    break;
+                case "portfolio":
+                    pageTittle = "PORTFOLIO";
+                    break;
+                case "contact":
+                    pageTittle = "CONTACTO";
+                    break;
+                default:
+                    pageTittle = string.Empty;
+                    break;
+            }
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+}
---
+++ Blazor-Portfolio/BlazorWASMPortfolio/Pages/Portfolio.razor.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorWASMPortfolio.Pages
+{
+    public partial class Portfolio
+    {
+    }
+}
