9eaf4220f3f6845d8c6f5f4f9948d1058aa4cbef
Matias <mgabrielpalacio@gmail.com>
2025-06-10 11:18:58 -0300

    aboutMe page

--- Blazor-Portfolio/BlazorWASMPortfolio/Pages/Aboutme.razor.cs
+++ Blazor-Portfolio/BlazorWASMPortfolio/Pages/Aboutme.razor.cs
@@ -1,6 +1,15 @@
-﻿namespace BlazorWASMPortfolio.Pages
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorWASMPortfolio.Pages
 {
     public partial class Aboutme
     {
+        [Inject] IJSRuntime Js { get; set;}
+
+        //protected override async Task OnAfterRenderAsync(bool firstRender)
+        //{
+        //    await Js.InvokeVoidAsync("startAllCarousel");
+        //}
     }
 }
--- Blazor-Portfolio/BlazorWASMPortfolio/wwwroot/Js/app.js
+++ Blazor-Portfolio/BlazorWASMPortfolio/wwwroot/Js/app.js
@@ -21,3 +21,14 @@
 //        });
 //    }, false);
 //})();
+
+function startAllCarousel = () => {
+    const carousels = document.querySelectorAll('.carousel');
+
+    carousels.forEach((element) => {
+        if (!element.dataset.bsCarouselInitialized) {
+            const carousel = new bootstrap.Carousel(element);
+            element.dataset.bsCarouselInitialized = "true";
+        }
+    });
+};
