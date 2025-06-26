d4b2dc9b2df90b7067f1769c1affff361d8a098f
Matias <mgabrielpalacio@gmail.com>
2025-06-26 14:38:47 -0300

    Skill Completed

--- Blazor-Portfolio/BlazorWASMPortfolio/Pages/Skills.razor.cs
+++ Blazor-Portfolio/BlazorWASMPortfolio/Pages/Skills.razor.cs
@@ -2,5 +2,40 @@
 {
     public partial class Skills
     {
+        private string frontCard = "expand";
+        private string backCard = "small";
+        private string othersCard = "small";
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ToggleSize(1);
+            await base.OnInitializedAsync();
+        }
+
+        private async Task ToggleSize(int cardId)
+        {
+            switch (cardId)
+            {
+                case 1:
+                    frontCard = "expand";
+                    backCard = "small";
+                    othersCard = "small";
+                    break;
+                case 2:
+                    frontCard = "small";
+                    backCard = "expand";
+                    othersCard = "small";
+                    break;
+                case 3:
+                    frontCard = "small";
+                    backCard = "small";
+                    othersCard = "expand";
+                    break;
+                default:
+                    break;
+            }
+
+            await InvokeAsync(StateHasChanged);
+        }
     }
 }
