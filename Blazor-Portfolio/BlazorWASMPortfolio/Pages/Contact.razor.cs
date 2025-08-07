3cf8aa902170598921b48921023030d14f11f54e
Matias <mgabrielpalacio@gmail.com>
2025-08-07 15:53:15 -0300

    Contact Page

--- Blazor-Portfolio/BlazorWASMPortfolio/Pages/Contact.razor.cs
+++ Blazor-Portfolio/BlazorWASMPortfolio/Pages/Contact.razor.cs
@@ -1,6 +1,40 @@
-﻿namespace BlazorWASMPortfolio.Pages
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace BlazorWASMPortfolio.Pages
 {
     public partial class Contact
     {
+        private ContactForm _contactForm = new ContactForm();
+
+        private bool _isInvalidForm = true;
+
+        private void ValidateForm()
+        {
+            _isInvalidForm = string.IsNullOrWhiteSpace(_contactForm.Name) ||
+                            string.IsNullOrWhiteSpace(_contactForm.LastName) ||
+                            string.IsNullOrWhiteSpace(_contactForm.Country) ||
+                            string.IsNullOrWhiteSpace(_contactForm.Email) ||
+                            !Regex.IsMatch(_contactForm.Email?? "aa", @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$") ||
+                            string.IsNullOrWhiteSpace(_contactForm.Query);            
+        }
+
+    }
+
+    public class ContactForm
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string? City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Correo inválido")]
+        public string Email { get; set; }
+        public string? Tel { get; set; }
+        [Required]
+        public string Query { get; set; }
     }
 }
